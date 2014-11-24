d5e8769d473834fa6ba37500f30a5e0746888d41
gulyasm <mgulyas86@gmail.com>
2014-11-24 10:28:20 +0100

    Changed Spark streaming example to Twitter stream.

--- spark/spark-streaming-wordcount/src/main/java/hu/dmlab/spark/SparkStreamingApplication.java
+++ spark/spark-streaming-wordcount/src/main/java/hu/dmlab/spark/SparkStreamingApplication.java
@@ -1,7 +1,11 @@
 package hu.dmlab.spark;
 
+import java.io.Serializable;
+import java.util.Arrays;
+
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.function.FlatMapFunction;
+import org.apache.spark.api.java.function.Function;
 import org.apache.spark.api.java.function.Function2;
 import org.apache.spark.api.java.function.PairFunction;
 import org.apache.spark.streaming.Duration;
@@ -9,46 +13,71 @@ import org.apache.spark.streaming.api.java.JavaDStream;
 import org.apache.spark.streaming.api.java.JavaPairDStream;
 import org.apache.spark.streaming.api.java.JavaReceiverInputDStream;
 import org.apache.spark.streaming.api.java.JavaStreamingContext;
-import scala.Tuple2;
+import org.apache.spark.streaming.twitter.TwitterUtils;
 
-import java.io.Serializable;
-import java.util.Arrays;
+import scala.Tuple2;
+import twitter4j.Status;
 
 public class SparkStreamingApplication implements Runnable, Serializable {
 
-    public static void main(String[] args) {
-        SparkStreamingApplication sparkStream = new SparkStreamingApplication();
-        sparkStream.run();
-    }
+	public static void main(String[] args) {
+		SparkStreamingApplication sparkStream = new SparkStreamingApplication();
+		sparkStream.run();
+	}
 
-    public void run() {
-        SparkConf conf = new SparkConf().setMaster("local[2]").setAppName("NetworkWordCount");
-        JavaStreamingContext jssc = new JavaStreamingContext(conf, new Duration(1000));
-        JavaReceiverInputDStream<String> lines = jssc.socketTextStream("localhost", 9999);
-        JavaDStream<String> words = lines.flatMap(new FlatMapFunction<String, String>() {
+	public void run() {
+		SparkConf conf = new SparkConf().setMaster("local[2]").setAppName(
+				"NetworkWordCount");
+		JavaStreamingContext jssc = new JavaStreamingContext(conf,
+				new Duration(1000));
+		System.setProperty("twitter4j.oauth.consumerKey",
+				"SdAaEy2NQxQsiVdqaoaNKhwNx");
+		System.setProperty("twitter4j.oauth.consumerSecret",
+				"rOMPFWLvlTXNPXRTAe9TJy1jEdupjd27TXZI2PGatAGSsrbSpN");
+		System.setProperty("twitter4j.oauth.accessToken",
+				"154070429-8PiYSmuniBiTuSeAl00YukhsBGzuu2DMb6LIpMvI");
+		System.setProperty("twitter4j.oauth.accessTokenSecret",
+				"DRXRaP0Jcaeruj8vbrwZJsny8EHO552YLZViyN1sfQYin");
+		JavaReceiverInputDStream<Status> lines = TwitterUtils
+				.createStream(jssc, new String[]{"bieber"});
+		JavaDStream<String> words = lines
+				.flatMap(new FlatMapFunction<Status, String>() {
 
-            public Iterable<String> call(String arg0) throws Exception {
-                return Arrays.asList(arg0.split(" "));
-            }
-        });
+					@Override
+					public Iterable<String> call(Status arg0) throws Exception {
+						return Arrays.asList(arg0.getText().split(" "));
+					}
+				});
 
-        JavaPairDStream<String, Integer> pairs = words.mapToPair(new PairFunction<String, String, Integer>() {
+		JavaPairDStream<String, Integer> pairs = words
+				.mapToPair(new PairFunction<String, String, Integer>() {
 
-            public Tuple2<String, Integer> call(String arg0) throws Exception {
-                return new Tuple2<>(arg0, 1);
-            }
-        });
+					public Tuple2<String, Integer> call(String arg0)
+							throws Exception {
+						return new Tuple2<>(arg0, 1);
+					}
+				});
 
-        JavaPairDStream<String, Integer> result = pairs.reduceByKey(new Function2<Integer, Integer, Integer>() {
+		JavaPairDStream<String, Integer> result = pairs
+				.reduceByKey(new Function2<Integer, Integer, Integer>() {
 
-            public Integer call(Integer arg0, Integer arg1) throws Exception {
-                return arg0 + arg1;
-            }
-        });
+					public Integer call(Integer arg0, Integer arg1)
+							throws Exception {
+						return arg0 + arg1;
+					}
+				});
 
-        result.print();
+		JavaPairDStream<String, Integer> filteredResult = result.filter(new Function<Tuple2<String,Integer>, Boolean>() {
 
-        jssc.start(); // Start the computation
-        jssc.awaitTermination(); // Wait for the computation to terminate
-    }
+			@Override
+			public Boolean call(Tuple2<String, Integer> arg0) throws Exception {
+				return arg0._2.intValue() > 2;
+			}
+		});
+
+		filteredResult.print();
+
+		jssc.start(); // Start the computation
+		jssc.awaitTermination(); // Wait for the computation to terminate
+	}
 }
